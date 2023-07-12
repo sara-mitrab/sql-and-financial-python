fc4304694ba3a843c5449e4374bc2e9ff87918e5
sara-mitrab <139122825+sara-mitrab@users.noreply.github.com>
2023-07-12 11:13:48 +0200

    Add files via upload
    In this file, I created a database with different tables around stocks and bonds, my aim is to create a financial oriented project, where I create a database, and use Peewee(ORM) and flask (API REST and client web) to manipulate data

---
+++ database.sql
@@ -0,0 +1,57 @@
+-- Table "actions"
+
+CREATE DATABASE python; 
+USE python;
+CREATE TABLE actions (
+  id INTEGER PRIMARY KEY,
+  nom TEXT,
+  prix REAL,
+  quantite INTEGER
+);
+
+-- Table "obligations"
+CREATE TABLE obligations (
+  id INTEGER PRIMARY KEY,
+  nom TEXT,
+  prix REAL,
+  taux_interet REAL
+);
+
+CREATE TABLE operations_sur_action (
+  action_id INTEGER PRIMARY KEY,
+  date DATE,
+  montant REAL,
+  FOREIGN KEY (action_id) REFERENCES actions (id)
+);
+
+
+CREATE TABLE operations_sur_obligation (
+  obligation_id INTEGER PRIMARY KEY,
+  date DATE,
+  montant REAL,
+  FOREIGN KEY (obligation_id) REFERENCES obligations (id)
+);
+
+
+INSERT INTO actions (id,nom, prix, quantite) VALUES
+    (1,'Action B', 15.2, 200),
+    (2,'Action C', 12.8, 150),
+    (3,'Action D', 8.5, 300);
+
+
+INSERT INTO obligations (id,nom, prix, taux_interet) VALUES
+    (1,'Obligation C', 600, 0.03),
+    (2,'Obligation D', 550, 0.02),
+    (3,'Obligation E', 700, 0.04);
+
+
+INSERT INTO operations_sur_action (action_id, date, montant) VALUES
+    (1, '2023-07-10', 1500),
+    (2, '2023-07-11', 2000),
+    (3, '2023-07-12', 1000);
+
+
+INSERT INTO operations_sur_obligation (obligation_id, date, montant) VALUES
+    (1, '2023-07-11', 500),
+    (2, '2023-07-12', 1000),
+    (3, '2023-07-13', 750);
