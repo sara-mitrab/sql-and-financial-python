ac448baa23f199af29d338cc1ca7301362373c27
sara-mitrab <139122825+sara-mitrab@users.noreply.github.com>
2023-07-18 09:39:50 +0200

    Add files via upload
    This code establishes a connection to a MySQL database and defines four model classes using the Peewee ORM. These model classes represent tables in the database and define the fields corresponding to the table columns. The code performs various queries to retrieve data from the tables and prints the results. It selects all actions, obligations with an interest rate greater than 3%, and operations on actions after a specific date. The code demonstrates basic database operations using Peewee for connecting, querying, and retrieving data from a MySQL database.

---
+++ model.py
@@ -0,0 +1,66 @@
+from peewee import *
+
+# Définir la connexion à la base de données
+db = MySQLDatabase('python', host='localhost', user='root', password='password')
+
+class Actions(Model):
+    id = AutoField()
+    nom = CharField()
+    prix = FloatField()
+    quantite = IntegerField()
+
+    class Meta:
+        database = db
+        table_name = 'actions'
+
+class Obligations(Model):
+    id = AutoField()
+    nom = CharField()
+    prix = FloatField()
+    taux_interet = FloatField()
+
+    class Meta:
+        database = db
+        table_name = 'obligations'
+
+class OperationsSurAction(Model):
+    action_id = ForeignKeyField(Actions, primary_key=True)
+    date = DateField()
+    montant = FloatField()
+
+    class Meta:
+        database = db
+        table_name = 'operations_sur_action'
+
+class OperationsSurObligation(Model):
+    obligation_id = ForeignKeyField(Obligations, primary_key=True)
+    date = DateField()
+    montant = FloatField()
+
+    class Meta:
+        database = db
+        table_name = 'operations_sur_obligation'
+
+
+db.connect()
+
+# Sélection de toutes les actions
+actions = Actions.select()
+print("voici toutes les actions \t")
+for action in actions:
+    print(action.nom, action.prix)
+
+# Sélection des obligations avec un taux d'intérêt supérieur à 3%
+obligations = Obligations.select().where(Obligations.taux_interet > 3)
+print("voici toutes les obligations avec un taux coupon supérieure à 3%")
+for obligation in obligations:
+    print(obligation.nom, obligation.prix, obligation.taux_interet)
+
+# Sélection des opérations sur actions effectuées après une certaine date
+import datetime
+
+date_limite = datetime.date(2022, 1, 1)
+operations_actions = OperationsSurAction.select().where(OperationsSurAction.date > date_limite)
+for operation in operations_actions:
+    print(operation.action_id.nom, operation.montant, operation.date)
+
