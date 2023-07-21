652bee11619e0dad8d4efa78510f0196b8ac6c19
sara-mitrab <139122825+sara-mitrab@users.noreply.github.com>
2023-07-21 10:03:02 +0200

    Update api.py

--- api.py
+++ api.py
@@ -18,6 +18,38 @@ def get_obligations():
     obligation_list = [{'nom': obligation.nom, 'prix': obligation.prix, 'taux_interet': obligation.taux_interet} for obligation in obligations]
     return jsonify(obligation_list)
 
+# Route pour créer une nouvelle action
+@app.route('/actions', methods=['POST'])
+def create_action():
+    data = request.json
+    action = Actions.create(nom=data['nom'], prix=data['prix'], quantite=data['quantite'])
+    return jsonify(model_to_dict(action))
+
+# Route to update an existing action
+@app.route('/actions/<int:action_id>', methods=['PUT'])
+def update_action(action_id):
+    data = request.json
+    try:
+        action = Actions.get(Actions.id == action_id)
+        action.nom = data['nom']
+        action.prix = data['prix']
+        action.quantite = data['quantite']
+        action.save()
+        return jsonify(model_to_dict(action))
+    except Actions.DoesNotExist:
+        return jsonify({'error': 'Action not found'}), 404
+
+# Route to delete an action
+@app.route('/actions/<int:action_id>', methods=['DELETE'])
+def delete_action(action_id):
+    try:
+        action = Actions.get(Actions.id == action_id)
+        action.delete_instance()
+        return jsonify({'message': 'Action deleted successfully'})
+    except Actions.DoesNotExist:
+        return jsonify({'error': 'Action not found'}), 404
+
+
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=9000)
 
