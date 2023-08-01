6f425dcdb707df9d717128721726935d24558c98
sara-mitrab <139122825+sara-mitrab@users.noreply.github.com>
2023-08-01 15:04:51 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 from peewee import *
 from model import Actions, Obligations, OperationsSurAction, OperationsSurObligation
 
@@ -14,42 +14,53 @@ def get_actions():
 # Route to retrieve obligations with interest rate greater than 3%
 @app.route('/obligations', methods=['GET'])
 def get_obligations():
-    obligations = Obligations.select().where(Obligations.taux_interet > 3)
+    obligations = Obligations.select()
     obligation_list = [{'nom': obligation.nom, 'prix': obligation.prix, 'taux_interet': obligation.taux_interet} for obligation in obligations]
     return jsonify(obligation_list)
 
-# Route pour créer une nouvelle action
+# Route to display the homepage with all actions and obligations
+@app.route('/', methods=['GET'])
+def index():
+    actions = Actions.select()
+    obligations = Obligations.select().where(Obligations.taux_interet > 3)
+    return render_template('index.html', actions=actions, obligations=obligations)
+
+# Route to add a new action
 @app.route('/actions', methods=['POST'])
-def create_action():
+def add_action():
     data = request.json
-    action = Actions.create(nom=data['nom'], prix=data['prix'], quantite=data['quantite'])
-    return jsonify(model_to_dict(action))
 
-# Route to update an existing action
-@app.route('/actions/<int:action_id>', methods=['PUT'])
-def update_action(action_id):
-    data = request.json
-    try:
-        action = Actions.get(Actions.id == action_id)
-        action.nom = data['nom']
-        action.prix = data['prix']
-        action.quantite = data['quantite']
-        action.save()
-        return jsonify(model_to_dict(action))
-    except Actions.DoesNotExist:
-        return jsonify({'error': 'Action not found'}), 404
+    if "nom" in data and "prix" in data and "quantite" in data:
+        nouvelle_action = {
+            "nom": data["nom"],
+            "prix": data["prix"],
+            "quantite": data["quantite"]
+        }
+        # You can save the nouvelle_action to your database or perform any other required action here.
 
-# Route to delete an action
+        return jsonify({"message": "Nouvelle action ajoutée avec succès !"}), 201
+    else:
+        return jsonify({"message": "Les données envoyées sont incomplètes."}), 400
+
+# Route to delete an existing action
 @app.route('/actions/<int:action_id>', methods=['DELETE'])
 def delete_action(action_id):
     try:
         action = Actions.get(Actions.id == action_id)
-        action.delete_instance()
-        return jsonify({'message': 'Action deleted successfully'})
     except Actions.DoesNotExist:
-        return jsonify({'error': 'Action not found'}), 404
+        return jsonify({"message": "L'action avec l'ID spécifié n'existe pas."}), 404
 
+    try:
+        action.delete_instance()
+        print("Action deleted:", action.id)
+        return jsonify({"message": "Action supprimée avec succès !"}), 200
+    except Exception as e:
+        print("Error deleting action:", str(e))
+        return jsonify({"message": "Une erreur s'est produite lors de la suppression de l'action.", "error": str(e)}), 500
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    return jsonify({"message": "Test route works!"})
 
 if __name__ == "__main__":
     app.run(port=9000)
-
